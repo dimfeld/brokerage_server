51ba437fffa560847619a49895d0a59da10f729e
Daniel Imfeld <daniel@danielimfeld.com>
2017-09-21 19:52:54 -1000

    Debug level configuration

--- types/plugin.go
+++ types/plugin.go
@@ -7,11 +7,21 @@ import (
 
 var ERR_PLUGIN_NOT_IMPLEMENTED = errors.New("Not implemented for this broker")
 
+type DebugLevel int
+
+const (
+	DEBUG_OFF     DebugLevel = 0
+	DEBUG_NORMAL             = 1
+	DEBUG_VERBOSE            = 2
+	DEBUG_TRACE              = 3
+)
+
 type BrokerageServerPluginV1 interface {
 	Connect() error
 	Close() error
 	Status() *ConnectionStatus
 	Error() error
+	SetDebugLevel(level DebugLevel)
 
 	// Accounts(ctx context.Context) ([]*Account, error)
 	GetStockQuote(ctx context.Context, symbol string) (*Quote, error)
