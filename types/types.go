46f0fa080efca302c6b1af9ea3b94faac822da80
Daniel Imfeld <daniel@danielimfeld.com>
2017-09-26 08:12:15 -1000

    Add omitempty to more Quote fields

--- types/types.go
+++ types/types.go
@@ -39,36 +39,36 @@ const (
 )
 
 type Quote struct {
-	High   float64 `json:"high"`
-	Low    float64 `json:"low"`
-	Open   float64 `json:"open"`
-	Close  float64 `json:"close"`
-	Mark   float64 `json:"mark"`
-	Volume int64   `json:"volume"`
+	High   float64 `json:"high,omitempty"`
+	Low    float64 `json:"low,omitempty"`
+	Open   float64 `json:"open,omitempty"`
+	Close  float64 `json:"close,omitempty"`
+	Mark   float64 `json:"mark,omitempty"`
+	Volume int64   `json:"volume,omitempty"`
 
-	Bid     float64 `json:"bid"`
-	BidSize int64   `json:"bid_size"`
-	BidExch string  `json:"bid_exch"`
+	Bid     float64 `json:"bid,omitempty"`
+	BidSize int64   `json:"bid_size,omitempty"`
+	BidExch string  `json:"bid_exch,omitempty"`
 
-	Ask     float64 `json:"ask"`
-	AskSize int64   `json:"ask_size"`
-	AskExch string  `json:"ask_exch"`
+	Ask     float64 `json:"ask,omitempty"`
+	AskSize int64   `json:"ask_size,omitempty"`
+	AskExch string  `json:"ask_exch,omitempty"`
 
-	LastTime time.Time `json:"last_time"`
-	Last     float64   `json:"last"`
-	LastSize int64     `json:"last_size"`
-	LastExch string    `json:"last_exch"`
+	LastTime time.Time `json:"last_time,omitempty"`
+	Last     float64   `json:"last,omitempty"`
+	LastSize int64     `json:"last_size,omitempty"`
+	LastExch string    `json:"last_exch,omitempty"`
 
-	OptionHistoricalVolatility float64 `json:"option_hv"`
-	OptionImpliedVolatility    float64 `json:"option_iv"`
-	OptionCallOpenInt          int64   `json:"option_call_open_int"`
-	OptionCallVolume           int64   `json:"option_call_vol"`
-	OptionPutOpenInt           int64   `json:"option_put_open_int"`
-	OptionPutVolume            int64   `json:"option_put_vol"`
+	OptionHistoricalVolatility float64 `json:"option_hv,omitempty"`
+	OptionImpliedVolatility    float64 `json:"option_iv,omitempty"`
+	OptionCallOpenInt          int64   `json:"option_call_open_int,omitempty"`
+	OptionCallVolume           int64   `json:"option_call_vol,omitempty"`
+	OptionPutOpenInt           int64   `json:"option_put_open_int,omitempty"`
+	OptionPutVolume            int64   `json:"option_put_vol,omitempty"`
 
 	// Shortable Tristate `json:"shortable"`
 
-	AvgVol float64 `json:"avg_vol"` // Not supported by all brokers
+	AvgVol float64 `json:"avg_vol,omitempty"` // Not supported by all brokers
 
 	// YearHigh float64 `json:"year_low"`
 	// YearLow  float64 `json:"year_high"`
@@ -85,6 +85,7 @@ type OptionQuote struct {
 	Type          PutOrCall `json:"type"`
 	MinPriceDelta float64   `json:"min_price_delta"`
 	FullSymbol    string    `json:"full_symbol"` // The full symbol for the option
+	OpenInterest  int64     `json:"open_interest"`
 
 	ModelPrice float64 `json:"model_price"`
 	Delta      float64 `json:"delta"`
