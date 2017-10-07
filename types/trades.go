c712b592d7cfabfc0db1f6378ef202715b517be6
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-06 14:15:12 -1000

    Restructure trades to account for multiple executions
    This also removes some info that the IB API was returning but
    which isn't relevant.

--- types/trades.go
+++ types/trades.go
@@ -2,25 +2,32 @@ package types
 
 import "time"
 
-type Trade struct {
-	Account string `json:"account"`
-	Broker  string `json:"broker"`
-	TradeId string `json:"trade_id"`
-
-	Symbol     string  `json:"symbol"`
-	OptionType string  `json:"type,omitempty"`
-	Strike     float64 `json:"strike,omitempty"`
-	Expiration string  `json:"expiration,omitempty"`
-	Multiplier string  `json:"multiplier,omitempty"`
+type Execution struct {
+	ExecutionId string  `json:"id"`
+	OptionType  string  `json:"type,omitempty"`
+	Strike      float64 `json:"strike,omitempty"`
+	Expiration  string  `json:"expiration,omitempty"`
+	Multiplier  string  `json:"multiplier,omitempty"`
 
 	Exchange    string  `json:"exchange"`
 	Side        string  `json:"side"`
 	Size        int     `json:"size"`
 	Price       float64 `json:"price"`
 	AvgPrice    float64 `json:"avg_price"`
-	CumQty      int64   `json:"cumulative_size"`
 	Commissions float64 `json:"commissions"`
 	RealizedPnL float64 `json:"realized_pnl,omitempty"`
+
+	Time    time.Time   `json:"time"`
+	RawData interface{} `json:"raw_data,omitempty"`
+}
+
+type Trade struct {
+	Account string `json:"account"`
+	Broker  string `json:"broker"`
+	OrderId string `json:"id"`
+	Symbol  string `json:"symbol"`
+
+	Executions []*Execution `json:"executions"`
 
 	Time    time.Time   `json:"time"`
 	RawData interface{} `json:"raw_data,omitempty"`
